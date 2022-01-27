da31d0106a93de5e680a4b62a6c9090d5478ffe8
kim-seungmin <ksm981204@naver.com>
2022-01-28 00:13:29 +0900

    19.getterSetter

---
+++ 19.getter,setter/Java100_18.java
@@ -0,0 +1,32 @@
+class car{
+	int speed;
+	int passenger;
+	String name;
+	car(String name){
+		this.name=name;
+	}
+	void printState(){
+		System.out.print(this.name+" "+this.speed+" "+this.passenger);
+	}
+
+	public String getName(){return name;} //getter
+	public void setName(String name){this.name=name;} //setter
+}
+class truck extends car{
+	int carryWeight;
+	truck(String name,int carryWeight){
+		super(name);
+		this.carryWeight=carryWeight;
+	}
+	void printState(){
+		super.printState();
+		System.out.print(" "+this.carryWeight);
+	}
+}
+class Java100_18{
+	public static void main(String[] args){
+		truck cyber = new truck("AOSLC",500);
+		cyber.setName("DSEA");
+		System.out.print(cyber.getName());
+	}
+}
