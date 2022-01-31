b64ca71b705e8e2062d8336bd51dbc66b4cb1e70
Kim-seung min <ksm981204@naver.com>
2022-01-31 13:07:18 +0000

    23 polymorphim

---
+++ 23.polymorphism/Java100_23.java
@@ -0,0 +1,11 @@
+class human{}
+class student extends human{
+	void sayHi(){System.out.println("HI");}
+}
+
+class Java100_23{
+	public static void main(String[] args){
+		human james = new human();
+		//james.sayHi();	err
+	}
+}
