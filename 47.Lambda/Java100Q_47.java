c990ae338776464a445711ddf210bdbef603c3f6
kim-seungmin <ksm981204@naver.com>
2022-03-03 23:39:47 +0900

    47 Lambda

--- 47.Lambda/Java100Q_47.java
+++ 47.Lambda/Java100Q_47.java
@@ -4,7 +4,7 @@ interface Calc { // 함수형 인터페이스의 선언
 } 
 
 public class Java100Q_47{
-public static void main(String[] args){
+	public static void main(String[] args){
         Calc minNum = (x, y) -> x < y ? x : y; // 추상 메소드의 구현
         System.out.println(minNum.min(3, 4));  // 함수형 인터페이스의 사용
     }
